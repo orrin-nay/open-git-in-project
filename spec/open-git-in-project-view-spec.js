f2567ce1f675c27d7addbf872eb4b9cd5114a086
theRealFakeRock <naylor.orrin@gmail.com>
2017-03-24 02:36:53 -0600

    finished

---
+++ spec/open-git-in-project-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OpenGitInProjectView from '../lib/open-git-in-project-view';
+
+describe('OpenGitInProjectView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
